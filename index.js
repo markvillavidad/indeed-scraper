197c4d51491d72c99663f49fb16108a1f06635a5
Mark Vidad <mvidad@pelmorex.com>
2020-07-13 18:39:16 -0400

    Fix

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 // Require our module.
 const IndeedService = require('scrape-indeed')();
+const https = require('follow-redirects').https;
+const jsdom = require("jsdom");
+const { v4: uuidv4 } = require('uuid');
+const timestamp = require('time-stamp');
+const { JSDOM } = jsdom;
+const fs = require('fs');
+
+const addTimeStamp = timestamp('YYYYMMDDHHmmssms');
+
+console.log('addTimeStamp', addTimeStamp);
 
 // Test with: node test.js 'Programmer' 'Vancouver' 25 50
 let options = {
@@ -10,13 +20,45 @@ let options = {
     count: process.argv[6]      // 50 job postings
 };
 
-console.log("Search options: ", options);
+const fileName = `${options.title}_${options.location}_${options.country}_${addTimeStamp}`;
 
 IndeedService.query(options)
-.then(function(data) {
-    // Do something with data ...
-    console.log("count", data.jobList.count);
-    console.log(data.jobList);
+.then(async function (data) {
+
+    let jobList = {};
+    jobList = data.jobList;
+   
+
+    await jobList.forEach(async element => {
+        element.key = await uuidv4();
+
+        await https.get("https://" + element.href, async (resp) => {
+            let data2 = '';
+          
+            resp.on('data', (chunk) => {
+                data2 += chunk;
+            });
+        
+            resp.on('end', () => {
+              const dom = new JSDOM(data2);
+              if(dom.window.document.getElementById("jobDescriptionText")) {
+                let description = dom.window.document.getElementById("jobDescriptionText").innerHTML;
+                element.description = description;
+              }
+              
+            });
+        
+        fs.writeFile(fileName, JSON.stringify(jobList, null, 2), function (err) {
+            if (err) throw err;
+            console.log('Saved!');
+        });
+        // console.log("jobList: ", jobList);
+
+        }).on("error", (err) => {
+            console.log("Error: " + err.message);
+        });
+    })
+
 })
 .catch(function(err) {
     console.log('Error: ' + err);
