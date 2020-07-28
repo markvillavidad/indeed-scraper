f0fbda0e40ada9450f64f7fd6e6db5d8dfa68401
Mark Vidad <mvidad@pelmorex.com>
2020-07-27 21:05:13 -0400

    TWM-0: Mehddd

--- index.js
+++ index.js
@@ -16,53 +16,127 @@ let options = {
     title: "nurse",     // Programmer
     location: "Toronto",  // Vancouver
     country: "Canada",   // Canada
-    radius: "100",    // 25 kilometer radius
-    count: "50"      // 50 job postings
+    radius: "50",    // 25 kilometer radius
+    count: "10"      // 50 job postings
 };
 
 const fileName = `${options.title}_${options.location}_${options.country}_${addTimeStamp}`;
 let jobList = {};
+let objectToWrite = [];
+
 
 IndeedService.query(options)
 .then(async function (data) {
-
-
     jobList = data.jobList;
    
+    // await jobList.forEach(async element => {
+    //     element.key = await uuidv4();
 
-    await jobList.forEach(async element => {
-        element.key = await uuidv4();
-
-        await https.get("https://" + element.href, async (resp) => {
-            let data2 = '';
-            let description  ="";
+    //     await https.get("https://" + element.href, async (resp) => {
+    //         let data2 = '';
+    //         let description  ="";
           
-            resp.on('data', (chunk) => {
-                data2 += chunk;
-            });
+    //         resp.on('data', (chunk) => {
+    //             data2 += chunk;
+    //         });
         
-            resp.on('end', () => {
-              const dom = new JSDOM(data2);
-              const jobDescription =  dom.window.document.getElementById("jobDescriptionText");
-              if(jobDescription) {
-                description = jobDescription.innerHTML;
-              }
-              element.description = description;
+    //         resp.on('end', () => {
+    //           const dom = new JSDOM(data2);
+    //           const jobDescription =  dom.window.document.getElementById("jobDescriptionText");
+    //           if(jobDescription) {
+    //             description = jobDescription.innerHTML;
+    //           }
+    //           element.description = description;
               
-            });
+    //         });
 
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
+    //     }).on("error", (err) => {
+    //         console.log("Error: " + err.message);
+    //     });
+    // })
+
+
+    const waitFor = (ms) => new Promise(r => setTimeout(r, ms))
+    const asyncForEach = async (array, callback) => {
+      for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array)
+      }
+    }
+
+    const getDescription = async (url) => {
+        return new Promise((resolve, reject) => {
+            https.get("https://" + url, resp => {
+                let data2 = '';
+            //     let description  ="";
+            
+                resp.on('data', (chunk) => {
+                    data2 += chunk;
+                });
+
+                resp.on('end', () => {
+                    try {
+                        const dom = new JSDOM(data2);
+                        const jobDescription =  dom.window.document.getElementById("jobDescriptionText");
+                        if(jobDescription) {
+                            resolve(jobDescription.innerHTML);
+                        }
+                  
+                    } catch (e) {
+                    reject(e.message);
+                    }
+                });
+            });
         });
-    })
+        
+        //     resp.on('end', () => {
+        //       const dom = new JSDOM(data2);
+        //       const jobDescription =  dom.window.document.getElementById("jobDescriptionText");
+        //       if(jobDescription) {
+        //         description = jobDescription.innerHTML;
+        //       }
+        //       element.description = description;
+              
+        //     });
+
+        // }).on("error", (err) => {
+        //     console.log("Error: " + err.message);
+        // });
+    }
+
+
+    
+    const start = async () => {
+      await asyncForEach(jobList, async (num) => {
+        num.description = await getDescription(num.href);
+        await waitFor(50);
+        console.log(num);
+        objectToWrite.push(num);
+        console.log("objectToWrite", objectToWrite);
+        await fs.writeFile(fileName, JSON.stringify(jobList, null, 2), function (err) {
+            if (err) throw err;
+            // console.log('Saved!');
+        });
+      
+      })
+    //   console.log('Done')
+    }
+    
+    start();
+        
+
+    
+
+
+
+
 
 })
 .then(async function () {
-    await fs.writeFile(fileName, JSON.stringify(jobList, null, 2), function (err) {
-        if (err) throw err;
-        // console.log('Saved!');
-    });
+    // await fs.writeFile(fileName, JSON.stringify(jobList, null, 2), function (err) {
+    //     if (err) throw err;
+    //     // console.log('Saved!');
+    // });
 } )
 .catch(function(err) {
-    console.log('Error: ' + err);
+    // console.log('Error: ' + err);
 });
